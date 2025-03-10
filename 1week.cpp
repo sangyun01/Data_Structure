1eb76060c990e35eae88b0424144a9cf110d8b12
sangyun01 <worldtop0612@gmail.com>
2025-03-11 00:52:43 +0900

    notebook git connect

--- 1week.cpp
+++ 1week.cpp
@@ -3,13 +3,27 @@
 
 using namespace std;
 
+enum Day
+{
+    SUN,
+    MON,
+    TUE,
+    WED,
+    THU,
+    FRI,
+    SAT
+};
+
 int main() {
     int x, y;
     cout << "please enter two numbers: ";
     cin >> x >> y;
     int sum = x + y;
 
+    Day today = THU;
+
     cout << "There sum is " << sum << endl;
+    cout << today << endl;
 
     return EXIT_SUCCESS;
 }
