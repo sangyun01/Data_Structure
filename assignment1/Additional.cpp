ed4c29a11fa188a71a100c244d7bc4a733cdfe62
sangyun01 <worldtop0612@gmail.com>
2025-04-14 14:49:21 +0900

    HW 1done

--- assignment1/Additional.cpp
+++ assignment1/Additional.cpp
@@ -4,34 +4,34 @@
 
 using namespace std;
 
-string input_value() {
-    string value;
-    cout << "Enter any string: ";
-    cin >> value;
-    return value;
+string input_value() {                  // function to get user input for a node element
+    string value;                       // define the variable type is string
+    cout << "Enter any string: ";   
+    cin >> value;                       // user input the value
+    return value;                       // return the value
 }
 
-int input_method() {
+int input_method() {                    // function to select a method (operation) to execute
     int num;
     cout << "Select the number" << endl;
-    cout << "(0 -> add / 1 -> remove)" << endl;
+    cout << "(0 -> add / 1 -> remove)" << endl;                                             
     cout << "(2 -> forward / 3 -> backward / 4 -> exit)" << endl;
-    cin >> num;
+    cin >> num;                         // user input the method number                
     return num;
 }
 
 void exe_method(int num, CDList &playList, const string &value) {
     if (num == 0) {
-        playList.add(value);
+        playList.add(value);    // input 0 -> add a new node after the cursor
     }
     else if (num == 1) {
-        playList.remove();
+        playList.remove();      // input 1 -> remove the node next to the cursor
     }
     else if (num == 2) {
-        playList.forward();
+        playList.forward();     // input 2 -> move the cursor to the next node
     }
     else if (num == 3) {
-        playList.backward();
+        playList.backward();    // input 3 -> move the cursor to the next node
     }
 }
 
@@ -43,22 +43,24 @@ int main() {
         num = input_method();
 
         if (num == 4) {
-            cout << "exit" << endl;
+            cout << "exit" << endl; // input 3 -> break the loop
             break;
         }
-        else if (num < 0 || num > 4) {
+        else if (num < 0 || num > 4) {  // if user input other number, then print error message
             cout << "Invalid number input" << endl;
-            continue;
+            continue;                   // repeat the loop
         }
 
-        string value = "";
+        string value = "";              // reset value
         if (num == 0) {
             value = input_value();
         }
 
-        exe_method(num, playList, value);
-        cout << playList << endl;
+        exe_method(num, playList, value);   // execute the selected method
+        cout << playList << endl;           // print the list
     }
+
+    getchar();
 
     return EXIT_SUCCESS;
 }
--- assignment1/CDList.h
+++ assignment1/CDList.h
@@ -11,32 +11,32 @@ typedef string Elem;
 
 class CDList;
 
-class CNode {   // define CNode class
+class CNode {               // define CNode class
     private:
-        Elem elem;
-        CNode *next;
-        CNode *prev;
+        Elem elem;          // define string type elem, elem is node element value
+        CNode *next;        // define node next point
+        CNode *prev;        // define node prev point
 
-        friend class CDList;
-        friend ostream &operator<<(ostream &out, const CDList &list);
+        friend class CDList;    // CDList to access the private members of CNode
+        friend ostream &operator<<(ostream &out, const CDList &list);   // operator<< to access private members for print
 };
 
 class CDList {  // define CDList
     public:
-        CDList();
-        ~CDList();
-        bool empty() const;
-        const Elem &front() const;
-        const Elem &back() const;
-        void forward();
-        void backward();
-        void add(const Elem &e);
-        void remove();
+        CDList();                   // constructor
+        ~CDList();                  // destructor
+        bool empty() const;         // check the list is empty
+        const Elem &front() const;  // return the element at front node
+        const Elem &back() const;   // return the element at cursor node
+        void forward();             // cursor move to next node
+        void backward();            // cursor move to prev node
+        void add(const Elem &e);    // add the node where the next to cursor
+        void remove();              // delete the node at the front node
 
         friend ostream &operator<<(ostream &out, const CDList &list);
-
+                                    // print the list
     private:
-        CNode *cursor;
+        CNode *cursor;              // pointer to current node
 };
 
 #endif
