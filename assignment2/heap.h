4080df57c0cc89d394ae261824c6620ef45fc331
sangyun01 <worldtop0612@gmail.com>
2025-05-24 21:43:26 +0900

    - mid, not commant the code

--- assignment2/heap.h
+++ assignment2/heap.h
@@ -11,14 +11,45 @@ struct Comparator {
 
 template <typename E>
 class VectorCompleteTree {
-private:
-    std::vector<E> V; 
-    .....
+    private:
+        std::vector<E> V; 
+    public:
+        typedef typename std::vector<E>::iterator Position;
+    protected:
+        Position pos(int i) { return V.begin() + i; }
+        int idx(const Position &p) const { return p - V.begin(); }
+    public:
+        VectorCompleteTree() : V(1) {}
+        int size() const { return V.size() - 1; }
+        Position left(const Position &p) { return pos(2 * idx(p)); }
+        Position right(const Position &p) { return pos(2 * idx(p) + 1); }
+        Position parent(const Position &p) { return pos(idx(p) / 2); }
+        bool hasLeft(const Position &p) const { return 2 * idx(p) <= size(); }
+        bool hasRight(const Position &p) const { return 2 * idx(p) + 1 <= size(); }
+        bool isRoot(const Position &p) const { return idx(p) == 1; }
+        Position root() { return pos(1); }
+        Position last() { return pos(size()); }
+        void addLast(const E &e) { V.push_back(e); }
+        void removeLast() { V.pop_back(); }
+        void swap(const Position &p, const Position &q) {
+            E e = *q;
+            *q = *p;
+            *p = e;
+        }
 };
 
 template <typename E, typename C>
 class HeapPriorityQueue {
-public:
-    .....
+    public:
+        int size() const;
+        bool empty() const;
+        void insert(const E &e);
+        const E &min();
+        void removeMin();
+    private:
+        VectorCompleteTree<E> T;
+        C isLess;
+
+        typedef typename VectorCompleteTree<E>::Position Position;
 };
 
