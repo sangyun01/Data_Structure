a33c6ea057c0cc3ca89d04e33763671d2fa241e2
sangyun01 <worldtop0612@gmail.com>
2025-05-31 00:11:07 +0900

    ...

--- assignment2/heap.cpp
+++ assignment2/heap.cpp
@@ -2,52 +2,52 @@
 
 template <typename E, typename C>
 int HeapPriorityQueue<E, C>::size() const {
-    return T.size();
+    return T.size();    // using Vector STL size function
 }
 
 template <typename E, typename C>
 bool HeapPriorityQueue<E, C>::empty() const {
-    return size() == 0;
+    return size() == 0; // when size is zero -> true
 }
 
 template <typename E, typename C>
 const E &HeapPriorityQueue<E, C>::min() {
-    return *(T.root());
+    return *(T.root()); // Dereferencing the T.root() is min element 
 }
 
 template <typename E, typename C>
 void HeapPriorityQueue<E, C>::insert(const E &e){
-    T.addLast(e);
-    Position v = T.last();
-    while(!T.isRoot(v)) {
-        Position u = T.parent(v);
-        if(!isLess(*v, *u))
+    T.addLast(e);   // push the lastly
+    Position v = T.last();  // set the iterator v
+    while(!T.isRoot(v)) {   // repeat the loop where root
+        Position u = T.parent(v);   // bring the v parent node u
+        if(!isLess(*v, *u)) // compare the element value.
             break;
-        T.swap(v, u);
-        v = u;
+        T.swap(v, u);   //if child node is small then swap the element
+        v = u;          // bring the iterator parent node.
     }
 }
 
 template <typename E, typename C>
 void HeapPriorityQueue<E, C>::removeMin() {
-    if(size() == 1)
+    if(size() == 1) // only has root
         T.removeLast();
     else {
-        Position u = T.root();
-        T.swap(u, T.last());
-        T.removeLast();
-        while (T.hasLeft(u)) {
-            Position v = T.left(u);
-            if (T.hasRight(u) && isLess(*(T.right(u)), *v))
-                v = T.right(u);
-            if (isLess(*v, *u)) {
-                T.swap(u, v);
-                u = v;
+        Position u = T.root();  // root iterator sets u
+        T.swap(u, T.last());    // swap the element value u and last element value
+        T.removeLast();         // then remove the before root value(min value)
+        while (T.hasLeft(u)) {  // if u has child (child left -> right)
+            Position v = T.left(u); // 1st set v is u's left child
+            if (T.hasRight(u) && isLess(*(T.right(u)), *v)) // if u also has right child then compare the left child and right child
+                v = T.right(u); // if right child element value more smaller then overwrite T.right(u)
+            if (isLess(*v, *u)) {   // compare the parent node and child node element value
+                T.swap(u, v);   // child node is more smaller than parent node, swap u, v
+                u = v;          // bring the iterator child node.
             }
             else break;
         }
     }
 }
 
-template class HeapPriorityQueue<int, Comparator<int>>;
-template class HeapPriorityQueue<char, Comparator<char>>;
+template class HeapPriorityQueue<int, Comparator<int>>; // using int type
+template class HeapPriorityQueue<char, Comparator<char>>; // using char type
--- assignment2/heap.h
+++ assignment2/heap.h
@@ -3,7 +3,7 @@
 #include <vector>
 
 template <typename E>
-struct Comparator {
+struct Comparator { // It compare the a and b
     bool operator()(const E& a, const E& b) const {
         return a < b;
     }
@@ -12,27 +12,27 @@ struct Comparator {
 template <typename E>
 class VectorCompleteTree {
     private:
-        std::vector<E> V; 
+        std::vector<E> V;   // using the STL vector
     public:
-        typedef typename std::vector<E>::iterator Position;
+        typedef typename std::vector<E>::iterator Position; // define the Postion replace the std::vector<E>::iterator
     protected:
-        Position pos(int i) { return V.begin() + i; }
-        int idx(const Position &p) const { return p - V.begin(); }
+        Position pos(int i) { return V.begin() + i; }       // Returns an iterator pointing to the i-th element
+        int idx(const Position &p) const { return p - V.begin(); } // reverse calculate idx by using the iterator p
     public:
-        VectorCompleteTree() : V(1) {}
-        int size() const { return V.size() - 1; }
-        Position left(const Position &p) { return pos(2 * idx(p)); }
-        Position right(const Position &p) { return pos(2 * idx(p) + 1); }
-        Position parent(const Position &p) { return pos(idx(p) / 2); }
-        bool hasLeft(const Position &p) const { return 2 * idx(p) <= size(); }
-        bool hasRight(const Position &p) const { return 2 * idx(p) + 1 <= size(); }
-        bool isRoot(const Position &p) const { return idx(p) == 1; }
-        Position root() { return pos(1); }
-        Position last() { return pos(size()); }
-        void addLast(const E &e) { V.push_back(e); }
-        void removeLast() { V.pop_back(); }
+        VectorCompleteTree() : V(1) {}              // constructor
+        int size() const { return V.size() - 1; }   // not using the index 0 => V.size()-1
+        Position left(const Position &p) { return pos(2 * idx(p)); }        // parent * 2 -> left child
+        Position right(const Position &p) { return pos(2 * idx(p) + 1); }   // parent * 2 + 1 -> right child
+        Position parent(const Position &p) { return pos(idx(p) / 2); }      // (left or right) child / 2 -> parent node
+        bool hasLeft(const Position &p) const { return 2 * idx(p) <= size(); }          // when has left child 2 * idx <= size -> if size -> 5 / p = 2 -> true , p = 3 -> false
+        bool hasRight(const Position &p) const { return 2 * idx(p) + 1 <= size(); }     // when has right child 2 * idx + 1 <= size -> if size -> 5 / p = 2 -> true , p = 3 -> false
+        bool isRoot(const Position &p) const { return idx(p) == 1; }        // root index is 1. indexing starts at 1
+        Position root() { return pos(1); }  // root idx -> 1
+        Position last() { return pos(size()); } // last idx same as size() value
+        void addLast(const E &e) { V.push_back(e); }    
+        void removeLast() { V.pop_back(); }             
         void swap(const Position &p, const Position &q) {
-            E e = *q;
+            E e = *q;   // e is temporary vaiable
             *q = *p;
             *p = e;
         }
@@ -47,8 +47,8 @@ class HeapPriorityQueue {
         const E &min();
         void removeMin();
     private:
-        VectorCompleteTree<E> T;
-        C isLess;
+        VectorCompleteTree<E> T;    // VectorCompleteTree based heap
+        C isLess;   // compare the C
 
         typedef typename VectorCompleteTree<E>::Position Position;
 };
